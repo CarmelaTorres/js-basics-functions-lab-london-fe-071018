5d677071b6a41e4034c596d52494f58071ccb3c3
Carmela Torres Molleda <carmela.torres@wework.com>
2018-08-14 11:03:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,15 +13,15 @@ function distanceTravelledInFeet(start, destination) {
   return feet
 }
 
-function calculatesFarePrice(start, destination) {
-  const feet = distanceTravelledInFeet(start, destination)
-  if (feet <= 400) {
-    return 0
-  } else if (feet > 400 && feet < 2000) {
-    return feet * 0.02
-  } else if (feet >= 2000 && feet < 2500) {
+calculatesFarePrice = (start, destination) => {
+  const distance = distanceTravelledInFeet(start, destination);
+  if (distance < 400) {
+    return 0;
+  } else if (distance > 400 && distance < 2000) {
+    return distance * .02;
+  } else if (distance > 2000 && distance < 2500) {
     return 25;
-  }  else {
-    return "cannot travel that far";
+  } else {
+    return 'cannot travel that far';
   }
-}
+} 
